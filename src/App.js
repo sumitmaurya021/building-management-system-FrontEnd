60acfe338baf3f6400972be382cb4b8b5160dcc4
Sumit Maurya <mauryasumit222@gmail.com>
2024-04-26 18:36:00 +0530

    [ 26 April ] [ Implementing login and sign up functionality ]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import GetAllBuildings from './Components/GetAllBuildings';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './Components/Navbar';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        {isLoggedIn && <Navbar />}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path='/signup' element={<SignUp setIsLoggedIn={setIsLoggedIn} />} />
+          {isLoggedIn && <Route path='/buildings' element={<GetAllBuildings />} />}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/Login.js
@@ -0,0 +1,193 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Login({ setIsLoggedIn }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [otpSent, setOTPSent] = useState(false);
+  const [otp, setOTP] = useState('');
+  const navigate = useNavigate();
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/login', {
+        user: {
+          email,
+          password
+        }
+      });
+      if (response.status === 200) {
+        // If login is successful, set OTP sent state to true
+        setOTPSent(true);
+        // Store user email
+        setEmail(response.data.email);
+        // Show toast notification for OTP sent
+        toast.info(`OTP sent to ${response.data.email}`);
+      }
+    } catch (error) {
+      setErrorMessage(error.response.data.error);
+    }
+  };
+
+  const handleOTPSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/verify_otp_and_login', {
+        user: {
+          email,
+          otp
+        },
+        client_id: 'aXfs_AWE1Gg-XkCiZ8LkY1xnZ3YNQSrvO18xfwVE8is'
+      });
+      if (response.status === 200) {
+        // Show toast notification for successful login
+        toast.success('Login successful!');
+        // Delay redirecting for 3 seconds
+        setTimeout(() => {
+          // Redirect to the dashboard or desired page after successful OTP verification
+          setIsLoggedIn(true);
+          navigate('/');
+        }, 2000);
+      }
+    } catch (error) {
+      setErrorMessage(error.response.data.error);
+    }
+  };
+  
+  
+
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={otpSent ? handleOTPSubmit : handleLoginSubmit} sx={{ mt: 1 }}>
+              {!otpSent ? (
+                <>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign In
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link href="#" variant="body2">
+                        Forgot password?
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link component={RouterLink} to="/signup" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </>
+              ) : (
+                <Box>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="otp"
+                    label="OTP"
+                    name="otp"
+                    type="text"
+                    autoComplete="off"
+                    value={otp}
+                    onChange={(e) => setOTP(e.target.value)}
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Verify OTP
+                  </Button>
+                </Box>
+              )}
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+      {/* Toast container for notifications */}
+      <ToastContainer />
+    </ThemeProvider>
+  );
+}
+
+export default Login;
